4f9253eb8150809b4494b1e037b8ba20f68cb7e3
JoseFaccirolli <jose.faccirolli08@gmail.com>
2026-09-04 17:04:54 -0300

    feat(esp32-led): Add blinkLed function to test I2C, I2C to Servo is working

--- Code/ESP32_Code_Canada/src/main.cpp
+++ Code/ESP32_Code_Canada/src/main.cpp
@@ -7,32 +7,56 @@ ServoKit servoKit;
 OpenMv openMv;
 
 // Pins and Address
+#define LED_PIN 2
 #define SERVO_PIN 5
 #define I2C_SDA 21
 #define I2C_SCL 22
 #define left_cam 0x12
 #define right_cam 0x14
 
+void blinkLed(uint8_t);
+// constexpr uint8_t Servo_Pin = 5;
+
 void setup() {
   Serial.begin(115200);
   openMv.begin(I2C_SDA, I2C_SCL, 100000);
-
+  servoKit.begin(SERVO_PIN);
+  servoKit.dropServoKit(2, 800, false, true);
+  pinMode(LED_PIN, OUTPUT);
   delay(100);
 }
 
 void loop() {
   // Query the victim
+  delay(1000);
   VictimResult result = openMv.getVictim(left_cam);
+  Serial.println(result.value);
+  Serial.println(result.confidence);
+  Serial.println(result.isTrue);
 
   if (result.isTrue) {
-    servoKit.dropServoKit(result.value, 800, true, true);
+    blinkLed(3);
+    servoKit.dropServoKit(result.value, 800, true, true); 
   }
   else {
     result = openMv.getVictim(right_cam);
 
     if (result.isTrue) {
+      blinkLed(3);
       servoKit.dropServoKit(result.value, 800, true, false);
     }
   }
 
+  // Serial.println("virando servo");
+  // servoKit.dropServoKit(2, 800, true, true);
+
+}
+
+void blinkLed(uint8_t times) {
+  for (byte i = 0; i < times; i++) {
+    digitalWrite(LED_PIN, HIGH);
+    delay(500);
+    digitalWrite(LED_PIN, LOW);
+    delay(500);  
+  }
 }
